b5de9caf95c8495b116d164bdcc9698158d769bc
Kobi Bar-Hanin <kobibarhanin@gmail.com>
2019-09-10 13:54:18 +0300

    update ui for landing and title

--- static/CalCendar.js
+++ static/CalCendar.js
@@ -10,15 +10,25 @@ routine_instances=[];
 
 calendar_instances=[];
 
+
+
 $(document).ready(function () {
-    initiate([], new Date().toISOString().split("T")[0])
+    initiate([], new Date().toISOString().split("T")[0], true)
 });
 
 // '2018-12-26'
 startDate=new Date(2018, 11, 23, 0, 0, 0, 0);
 endDate=new Date(2018, 11, 29, 0, 0, 0, 0);
 
-function initiate(events,defaultDate) {
+function initiate(events, defaultDate, first) {
+
+    if (first){
+        $("#routine_populated").hide()
+        $("#opportune_populated").hide()
+        $("#floating_populated").hide()
+        $("#anchor_populated").hide()
+        $("#events_populated").hide()
+    }
 
     $('#rangestart').calendar({
         type: 'date',
@@ -57,6 +67,7 @@ function initiate(events,defaultDate) {
 
     if (window.innerWidth < 768 ) {
         $('#calendar').fullCalendar('changeView', 'agendaDay');
+
     }
 
     $("#dev_button").unbind('click').click(function(){
@@ -101,7 +112,7 @@ function initiate(events,defaultDate) {
                     $("#datepicker").after("<div id='calendar' style='max-width: none'></div>");
                     calendar_instances = instances
                     $("#calc_button").removeClass("loading");
-                    initiate(instances,endDate.toISOString().split("T")[0]);
+                    initiate(instances,endDate.toISOString().split("T")[0], false);
                     $("html, body").animate({ scrollTop: 0 }, "slow");
           });
     });
@@ -122,12 +133,15 @@ function initiate(events,defaultDate) {
                    instances_golbal_mapping[instances_golbal[i]['id']]=i;
                 }
 
-                initiate(instances,endDate.toISOString().split("T")[0]);
+                switcher("events_populated","events_default")
+                initiate(instances,endDate.toISOString().split("T")[0], false);
                 build_table(instances_golbal,'events_table')
         });
     });
 
     $("#routine_button").unbind('click').click(function(){
+
+        switcher("routine_populated","routine_default")
 
         time_from = new Date($("#routine_from").val()).getTime()/1000
         time_to = new Date($("#routine_to").val()).getTime()/1000
@@ -137,20 +151,21 @@ function initiate(events,defaultDate) {
 
         var table = document.getElementById('routine_table');
         var row = table.insertRow(table.rows.length);
-        row.id = title+duration
+        row.id = title+"_"+Math.random().toString(36).substring(7);
         var cell1 = row.insertCell(0);
         var cell2 = row.insertCell(1);
         var cell3 = row.insertCell(2);
         var cell4 = row.insertCell(3);
         var cell5 = row.insertCell(4);
+
         cell1.innerHTML = title
         cell2.innerHTML = duration
-        cell3.innerHTML = time_from
-        cell4.innerHTML = time_to
-        cell5.innerHTML = "<input id=cb_"+title+duration+" type='checkbox' name="+title+duration+" value='some_value'>"
+        cell3.innerHTML = new Date(time_from*1000).toISOString()
+        cell4.innerHTML = new Date(time_to*1000).toISOString()
+        cell5.innerHTML = "<input id=cb_"+row.id+" type='checkbox' name=cb_"+title+duration+" value='some_value'>"
 
         var routine_instance = {
-            "id": title,
+            "id": row.id,
             "title": title,
             "duration": duration,
             "time_from": time_from,
@@ -207,14 +222,17 @@ function add_to_row_table(entry,table_id) {
 
 
     if (table_id === "anchor_table"){
+        switcher("anchor_populated","anchor_default")
         let idx = instances_golbal_mapping[entry["id"]];
         anchor_instances.push(instances_golbal[idx])
     }
     else if (table_id === "floating_table") {
+        switcher("floating_populated","floating_default")
         let idx = instances_golbal_mapping[entry["id"]];
         floating_instances.push(instances_golbal[idx])
     }
     else if (table_id === "opportune_table") {
+        switcher("opportune_populated","opportune_default")
         let idx = instances_golbal_mapping[entry["id"]];
         opportune_instances.push(instances_golbal[idx])
     }
@@ -239,6 +257,16 @@ function remove_selected(table_id) {
             add_to_row_table(clear_list[i],'events_table')
         }
     }
+
+    if (opportune_instances.length == 0){
+        switcher("opportune_default", "opportune_populated")
+    }
+    if (floating_instances.length == 0){
+        switcher("floating_default", "floating_populated")
+    }
+    if (anchor_instances.length == 0){
+        switcher("anchor_default", "anchor_populated")
+    }
 }
 
 
@@ -253,6 +281,12 @@ function remove_selected_routing() {
             routine_instances.splice(i, 1);
         }
     }
+
+    if (routine_instances.length == 0){
+        switcher("routine_default", "routine_populated")
+    }
+
+
 }
 
 function arrayToString(array){
@@ -269,4 +303,9 @@ function routienArrayToString(array){
         res += array[i]["title"]+";"+array[i]["duration"]+";"+array[i]["time_from"]+";"+array[i]["time_to"]+","
     }
     return res
+}
+
+function switcher(on, off){
+    $("#"+off).hide()
+    $("#"+on).show()
 }
